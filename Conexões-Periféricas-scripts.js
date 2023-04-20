31529b9bf5155844e6b2915073dbaa5e467ab98a
Thifanny Celine <128502624+thifanny-tif@users.noreply.github.com>
2023-04-20 00:02:42 -0300

    estudando JS

---
+++ Conexões-Periféricas-scripts.js
@@ -0,0 +1,86 @@
+//----------------------------------------------------------------------------------17/abril
+//nome do arquivo Script.js
+//como adicinonar no html: antes do fim de BODY <script src = "script.js"></script>
+
+//----------------------------------------------------------------------------------
+
+//alert("vamo")
+//console.log("vamo")
+
+//----------------------------------------------------------------------------------
+
+//var idade = "15"
+//console.log(idade)
+
+//----------------------------------------------------------------------------------
+
+//var num1=5
+//var num2=19
+//var soma = num1+num2
+//console.log(soma)
+
+//----------------------------------------------------------------------------------
+
+//var meunumero = 2243
+//var sobrenome = "neto"
+//console.log(meunumero + sobrenome)
+
+//----------------------------------------------------------------------------------19/abril
+//calcule a area e o perimetro do retangulo. area=base+altura
+
+//var base = 5
+//var altura = 8
+//var area = base*altura
+//console.log(area)
+
+//descubra a area do triangulo
+
+//var base = 3
+//var altura = 4
+//var area = (base*altura)/2
+//console.log(area)
+
+//M=C*(1+i)^t onde i=2% ou 2/100 ou 0,02 -----------------------------------------------DEU ERRO
+
+//var C = 4
+//var i = 2
+//var t = 3
+//var M = C*math.pow((1+i),t);
+//console.log(M)
+
+//Concatenação ()
+//valor STRIG _ texto deve vir sempre entre "aspas"--------------------------------------
+
+//var c = "conexoes"
+//var p = "perifericas"
+//var r = (c + p)
+//console.log(r)
+
+//var conexoes = "conexoes"
+//var perifericas = "perifericas"
+//var resultado = (conexoes + perifericas)
+//console.log(resultado)
+
+//Number + Strig -------------------------------------------------------------------------
+//para mostrar que um numero é um texto "5" e um numero é um numero 5
+//resultado correto: 55
+//resultado errado: 10
+
+//var n1 = 5
+//var n2 = "5"
+//var contat=(n1+n2)
+//console.log(contat)
+
+//var p1 = "conexoes "
+//var p2 = "é show "
+//var p3 = "de bola!"
+//var strig = (p1+p2+p3)
+//console.log(strig)
+
+--------------------------------------------------------------------------------------------ESTUDAR MAIS
+
+//var p1 = "conexoes"
+//var p2 = "show"
+//var p3 = "bola!"
+//var strig = (p1+p2+p3)
+//console.log(strig)
